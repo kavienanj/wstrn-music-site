246f99efe2de975cb3ac8159b1b8e98185a664ff
Kavienan J <kavienanj@gmail.com>
2020-06-13 16:00:56 +0530

    configured article page structure

--- js/article.js
+++ js/article.js
@@ -1,12 +1,15 @@
 function article(title) {
-    if (title != null) {
-        getArticlePage(title);
-    } else {
-        getArticlePage('404 Not Found');
-    }
+    getArticlePage(title, readFile("pages/"+title+".html"));
 }
 
-function getArticlePage(title) {
+function getArticlePage(title, body) {
     var titleElement = document.getElementById('title');
-    titleElement.innerHTML = title;
+    var bodyElement = document.getElementById('contentBody');
+    if (body != null && title != null) {
+      titleElement.innerHTML = title;
+      bodyElement.innerHTML = body;  
+    } else {
+      titleElement.innerHTML = "404 Not Found";
+      bodyElement.innerHTML = "";
+    }
 }
